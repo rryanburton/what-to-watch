81590fc00d60d725c5708fff91de7d450d4edc68
Ryan Burton <rryanburton@gmail.com>
2015-09-13 17:45:04 -0400

    added data file import functions

--- movie_lib.py
+++ movie_lib.py
@@ -1,5 +1,5 @@
 import csv
-
+import math
 all_movies = {}
 all_users = {}
 
@@ -12,11 +12,19 @@ class User:
         # occupation
         # zipcode
         all_users[self.id] = self
+        self.ratings = {}  #key: movie_id, value: rating stars
 
-        def __str__(self):
-            return 'User(user_id={})'.format(self.id)
-        def __repr__(self):
-            return self.__str__()
+    def add_user_rating(self, rating):
+        self.ratings[rating.movie] = rating.stars
+
+    def get_user_ratings(self):
+        return self.ratings
+
+
+        # def __str__(self):
+        #     return 'User(user_id={})'.format(self.id)
+        # def __repr__(self):
+        #     return self.__str__()
 
 
 class Movie:
@@ -24,6 +32,7 @@ class Movie:
         self.id = movie_id
         self.title = title
         all_movies[self.id] = self
+
         self.ratings = {}  #key: user_id, value: Rating object
 
         # several other columns of info
@@ -35,26 +44,30 @@ class Movie:
         return self.__str__()
 
     def add_rating(self, rating):
-        self.ratings[rating.user_id] = rating
+        self.ratings[rating.user] = rating.stars
 
     def get_ratings(self):
         return self.ratings.values()
 
+    def get_ave_rating(self):
+        return sum(self.get_ratings())/len(self.get_ratings())
 
     def get_title(self):
         return self.title
 
-    def get_id(title):
-        return movie_id
+    # def get_id(title):
+    #     return movie_id
 
 
 class Rating:
-    def __init__(self, user_id, movie_id, stars):
-        self.user_id = user_id
-        self.movie_id = movie_id
+    def __init__(self, user, movie, stars):
+        self.user = user
+        self.movie = movie
         self.stars = stars
         # self.timestamp = timestamp
-        all_movies[self.movie_id].add_rating(self)
+        all_movies[self.movie].add_rating(self)
+        all_users[self.user].add_user_rating(self)
+
 
     def __str__(self):
         return 'Rating(user_id={}, movie_id={}, stars={})'.format(self.user_id, self.movie_id, self.stars)
@@ -63,71 +76,31 @@ class Rating:
         return self.__str__()
 
 
-#Find all ratings for a movie by id
 
 
-##Find the average rating for a movie by id
-#Find all ratings for a movie by id
-# then find the average rating for the matching rows
-#
+def get_ratings():
+    with open('ml-100k/u.data') as args:
+        reader = csv.DictReader(args, fieldnames=['user_id', 'movie_id', 'rating'], delimiter='\t')
+        for row in reader:
+            Rating(int(row['user_id']), int(row['movie_id']), int(row['rating']))
 
-##find all ratings for a User
-# #look for all instances of the user id in u.data of the user id in u.user
+def get_items():
+    with open('ml-100k/u.item', encoding='latin-1') as args:
+        reader = csv.DictReader(args,  fieldnames=['movie_id', 'movie_title', ], delimiter='|')
+        for row in reader:
+            Movie(int(row['movie_id']), row['movie_title'])
 
-#
-# with open('u.data', 'rt') as args:
-# with open('testdata.csv', 'rt') as args:
-#     cin = csv.reader(args, delimiter='\t')
-#     movie_ratings = [row for row in cin]
-# print(movie_ratings)
-#
-# with open('testdata.csv') as f:
-#     reader = csv.reader(f, delimiter='\t')
-#     # header = next(reader)
-#     for row in reader:
-#         print(row)
-#
-# with open('testdata.csv') as f:
-#     reader = csv.DictReader(f, delimiter='\t')
-#     for row in reader:
-#         print(row)
+def get_users():
+    with open('ml-100k/u.user') as args:
+        reader = csv.DictReader(args, fieldnames=['user_id'], delimiter='|')
+        for row in reader:
+            User(int(row['user_id']))
 
-# with open('testdata.csv') as f:
-#     reader = csv.DictReader(f, delimiter='\t', fieldnames=['user_id', 'movie_id', 'stars', 'timestamp'])
-#     for row in reader:
-#         print(row)
+def get_data():
+    get_items()
+    get_users()
+    get_ratings()
 
-
-
-# with open('u.item') as args:
-#     cin = csv.reader(args, delimiter='|')
-#     movie_info = [row for row in cin]
-# print(movie_info)
-#
-#
-# with open('u.user') as args:
-#     cin = csv.reader(args, delimiter='|')
-#     users = [row for row in cin]
-# print(users)
-
-
-# class ClassName(object):
-#     """docstring for """
-#     def __init__(self, arg):
-#         super(, self).__init__()
-#         self.arg = arg
-#
-#lookup movie_id when given movie_title
-#find the name of the movie in u.item by looking up a particular movie_id
-
-##find all ratings for a User
-
-#   Find the name of a movie by id
-
-
-#   Find all ratings for a movie by id
-#   all_movies[movie1.id].get_ratings()
-#   returns this:  dict_values([Rating(user_id=12, movie_id=9, stars=3), Rating(user_id=5, movie_id=9, stars=1)])
 def top_movies():
     num_top = input("how many top movies do you want to see?\n")
     min_num_top = input("what is the minimum number of ratings a movie must have to be considered in the top list? \n")
@@ -140,3 +113,23 @@ def top_movies():
     print("""The top {} movies by average rating are:\n
         disclaimer: movies must have been ranked at
          least {} times.\n""".format(num_top,min_num_top))
+
+# for each movie in top_movies look for a user_id and
+# remove those movie_ids and make a new top_movies list
+
+
+
+            # class ClassName(object):
+            #     """docstring for """
+            #     def __init__(self, arg):
+            #         super(, self).__init__()
+            #         self.arg = arg
+
+
+#   Find all ratings for a movie by id
+#   all_movies[movie1.id].get_ratings()
+#   returns this:  dict_values([3, 1, etc])
+
+#   Find average rating for movie by id
+#   all_movies[movie2.id].get_ave_rating()
+#   returns this: 2.0  (float ave)
